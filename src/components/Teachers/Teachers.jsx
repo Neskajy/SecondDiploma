194fea65458a442367d65f4268968d18edf9bfbe
unknown <besonnovskiyyy@gmail.com>
2025-07-25 00:34:37 +0300

    added scroll for teachers block

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -1,15 +1,18 @@
 import s from "./Teachers.module.scss";
 import user404 from "../../assets/imgs/vector/user.svg";
-
+import { useRef } from "react";
 
 export default function Teachers() {
+    const teacher__card = useRef();
+
     return (
         <>
             <section className={s.Teachers}>
                 <div className={s.container}>
                     <h3>Наши преподаватели</h3>
                     <div className={s.grid}>
-                        <div className={s.teacher__card}>
+                        
+                        <div className={`${s.teacher__card} ${s.active}`} ref={teacher__card}>
                             <div className={s.img}>
                                 <img src={user404} alt="" />
                             </div>
